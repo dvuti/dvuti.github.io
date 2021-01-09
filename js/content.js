2ccc8c3c9910626deed4b565676c038a67adbf66
dvuti <77163431+dvuti@users.noreply.github.com>
2021-01-09 11:07:52 +0300

    Update content.js

--- js/content.js
+++ js/content.js
@@ -1,1 +1,16 @@
+const nickname = document.getElementById("nickname");
+const password = document.getElementById("password");
+const id = document.getElementById("id");
 
+const addbtn = document.getElementById("addbutton");
+
+const database = firebase.database();
+
+addbtn.addEventListener('click', (e) => {
+  e.peventDefault();
+  database.ref('/user/' +  id.value).set({
+    nickname: nick.value,
+    pass: password.value
+    
+  });
+});
