d21a47541c663135845a577566ce3aedcd83e17b
dvuti <77163431+dvuti@users.noreply.github.com>
2021-01-09 20:11:04 +0300

    Update content.js

--- js/content.js
+++ js/content.js
@@ -1,1 +1,6 @@
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
 
+          alert("Вы используете мобильное устройство (телефон или планшет).")
+          document.location.href = "https://dvuti.github.io/mobile_version";
+        }
+         else alert("Вы используете ПК.")
