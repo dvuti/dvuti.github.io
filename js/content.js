67b303368107c3614e147dc92176f3ab548a5186
dvuti <77163431+dvuti@users.noreply.github.com>
2021-01-10 13:53:56 +0300

    Update content.js

--- js/content.js
+++ js/content.js
@@ -4,3 +4,12 @@ if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phon
           document.location.href = "https://dvuti.github.io/mobile_version";
         }
          else alert("Вы используете ПК.")
+
+
+      function check(){
+        if(document.getElementById('passbox').value == document.getElementById('password1').value){
+          alert('Пароли верны');
+        } else {
+          alert('Пароли неверны');
+        }
+      }
