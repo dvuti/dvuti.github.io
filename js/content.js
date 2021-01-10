b8a6ef7c3e4b983b25f2cf6d95cea1ba8a5ae215
dvuti <77163431+dvuti@users.noreply.github.com>
2021-01-10 15:13:13 +0300

    Update content.js

--- js/content.js
+++ js/content.js
@@ -4,12 +4,3 @@ if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phon
 } else {
           alert("Вы используете ПК.")
 }
-
-
-function check(){
-          if(document.getElementById('passbox').value == document.getElementById('password1').value){
-                    alert('Пароли верны');
-          } else {
-                    alert('Пароли неверны');
-          }
-}
