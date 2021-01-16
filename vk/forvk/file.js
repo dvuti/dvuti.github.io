6cf82678e105d8eda276e52cf9073ab7d2b60e24
dvuti <77163431+dvuti@users.noreply.github.com>
2021-01-16 14:15:49 +0300

    Update file.js

--- vk/forvk/file.js
+++ vk/forvk/file.js
@@ -1,24 +1,24 @@
 
 var clicks = 0;
 document.getElementById('lol').onclick = function(){
-clicks = clicks + 1,
-input = document.getElementById('block'); // целевой тег <input>
-block.value = clicks;
-document.getElementById('vow').innerHTML = clicks;
-        }
-        var script = document.createElement('SCRIPT');
-        script.src="https://api.vk.com/method/users.get?access_token=25b2c9f38cbe4257eccb3861cd97af61ee8fea7015899a7070abce98f76971e048782291589d1141bd837&v=5.126&callback=callbackFunc";
-        document.getElementsByTagName("head")[0].appendChild(script);
-        function callbackFunc(result) {
-          idc = result.response[0].id
-        }
+        clicks = clicks + 1,
+                input = document.getElementById('block'); // целевой тег <input>
+        block.value = clicks;
+        document.getElementById('vow').innerHTML = clicks;
+}
+var script = document.createElement('SCRIPT');
+script.src="https://api.vk.com/method/users.get?access_token=25b2c9f38cbe4257eccb3861cd97af61ee8fea7015899a7070abce98f76971e048782291589d1141bd837&v=5.126&callback=callbackFunc";
+document.getElementsByTagName("head")[0].appendChild(script);
+function callbackFunc(result) {
+        idc = result.response[0].id
+}
 
-script src="https://www.gstatic.com/firebasejs/8.2.2/firebase-app.js"
-script src="https://www.gstatic.com/firebasejs/8.2.2/firebase-auth.js"
-script src="https://www.gstatic.com/firebasejs/8.2.2/firebase-database.js"
+script.src="https://www.gstatic.com/firebasejs/8.2.2/firebase-app.js"
+script.src="https://www.gstatic.com/firebasejs/8.2.2/firebase-auth.js"
+script.src="https://www.gstatic.com/firebasejs/8.2.2/firebase-database.js"
 
       
-      var firebaseConfig = {
+var firebaseConfig = {
         apiKey: "AIzaSyCturT2bd0DyL6dnP67npvLhqktJ0ECL4Q",
         authDomain: "dvuti-7ede1.firebaseapp.com",
         databaseURL: "https://dvuti-7ede1-default-rtdb.firebaseio.com",
@@ -27,20 +27,19 @@ script src="https://www.gstatic.com/firebasejs/8.2.2/firebase-database.js"
         messagingSenderId: "27719067783",
         appId: "1:27719067783:web:89fab657d78300bb577022",
         measurementId: "G-ZRHD3011G9"
-      };
-      firebase.initializeApp(firebaseConfig);
+};
+firebase.initializeApp(firebaseConfig);
 
-      // сохранение
-      document.getElementById('save').onclick = function(){
+// сохранение
+document.getElementById('save').onclick = function(){
         firebase.database().ref('profils/'+idc).set({
-          click: clicks,
-          id_chels: idc
+                click: clicks,
+                id_chels: idc
         });
-      }
-      //получение данных
-      document.getElementById('rosave').onclick = function(){
+}
+//получение данных
+document.getElementById('rosave').onclick = function(){
         firebase.database().ref('profils/'+idc).on('value',function(snapshot){
-          clicks= snapshot.val().click;
-
-  });
+                clicks= snapshot.val().click;
+        });
 }
